bc6c90a73517c01ee11fa40bd231e2a831743c63
Henri du Plessis <henriduplessis99@gmail.com>
2023-11-02 15:47:08 +0200

    Downloaded first 100 match pages

--- main.py
+++ main.py
@@ -3,31 +3,36 @@ from database.setup import session, create_tables
 from database.operations import get_match_urls, update_match_url
 from download import download_pages
 
-def main():
-    # create_tables()
+def main(create_tables = False,
+         download_matches = False):
 
-    matches_to_download = get_match_urls(downloaded=False, limit=1)
-    download_list = []
-    for match_ in matches_to_download:
-        download_list.append({
-            'url' : f"https://www.hltv.org/matches/{match_.id}/{match_.url}",
-            'path' : f"match/{match_.id}"
-        })
+    if create_tables:
+        create_tables()
+
+    # Get list of matches to download
+    matches_to_download = get_match_urls(downloaded=False, limit=98)
+    download_list = [
+        {
+            'url': f"https://www.hltv.org/matches/{match.id}/{match.url}",
+            'path': f"match/{match.id}"
+        }
+        for match in matches_to_download
+    ]
+
+    # Download the match pages and update the 'downloaded' flag in database
     success = download_pages(download_list)
     success_ids = [int(match.split("/")[4]) for match in success]
     update_match_url(success_ids, 'downloaded')
 
+    # Get list of matches to process
+    matches_to_process = get_match_urls(processed=False)
+    for match in matches_to_process:
+        print("...")
+        # Extract match info, save to 
+    # Store in db
+    # Flag as processed
 
-
-
-    # Use the MatchURL model and session to perform database operations
-    # new_match = MatchURL(id = 12345, url='https://example.com', downloaded=False, processed=False)
-    # session.add(new_match)
-    # session.commit()
-
-    # 1. Download new pages
-    # 2. Scrape downloaded pages
-    # 3. Store in database
+    
 
 if __name__ == '__main__':
     main()
