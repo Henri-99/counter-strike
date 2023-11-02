3e6ccc0683a2e57dadd01b32ceec6864263372d4
Henri du Plessis <henriduplessis99@gmail.com>
2023-11-02 14:05:09 +0200

    Added match_url table

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from models.match_url import MatchURL
+from database import session, create_tables
 
 def main():
-    print("does nothing")
+    create_tables()
+
+    # Use the MatchURL model and session to perform database operations
+    new_match = MatchURL(id = 12345, url='https://example.com', downloaded=False, processed=False)
+    session.add(new_match)
+    session.commit()
 
     # 1. Download new pages
     # 2. Scrape downloaded pages
     # 3. Store in database
-
 
 if __name__ == '__main__':
     main()
