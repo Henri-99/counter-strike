f4f8092f37eb78c88ecb9c73d5301adff578a4cf
Henri du Plessis <henriduplessis99@gmail.com>
2023-11-13 16:10:46 +0200

    Added winner rank differential distribution

--- analytics.py
+++ analytics.py
@@ -42,8 +42,18 @@ def get_matches(start_date=None, end_date=None, limit=None):
 
 	return match_data
 
-def get_maps_count(start_date=None, end_date=None):
+def get_maps_count(start_date=None, end_date=None, cs2=None, lan=None):
 	query = session.query(Map)
+
+	if cs2 is not None or lan is not None:
+		query = query.join(Match, Map.match_id == Match.id)
+
+	if cs2 is not None:
+		query = query.filter(Match.cs2 == 1 if cs2 else Match.cs2 == 0)
+
+	if lan is not None:
+		query = query.filter(Match.lan == 1 if lan else Match.lan == 0)
+
 	if start_date and end_date is not None:
 		query = query.filter(
 			and_(
@@ -56,8 +66,15 @@ def get_maps_count(start_date=None, end_date=None):
 	
 	return map_count
 
-def get_matches_count(start_date=None, end_date=None):
+def get_matches_count(start_date=None, end_date=None, cs2=None, lan=None):
 	query = session.query(Match)
+
+	if cs2 is not None:
+		query = query.filter(Match.cs2 == 1 if cs2 else Match.cs2 == 0)
+
+	if lan is not None:
+		query = query.filter(Match.lan == 1 if lan else Match.lan == 0)
+
 	if start_date and end_date is not None:
 		query = query.filter(
 			and_(
@@ -70,7 +87,7 @@ def get_matches_count(start_date=None, end_date=None):
 	
 	return match_count
 
-def records_per_month():
+def records_per_month(cs2 = None, lan = None):
 	data = []
 	start_date, end_date = get_date_range()
 	
@@ -89,8 +106,8 @@ def records_per_month():
 	date_pairs = [(first_days[i], first_days[i + 1]) for i in range(len(first_days) - 1)]
 
 	for start, end in date_pairs:
-		maps = get_maps_count(start, end)
-		matches = get_matches_count(start, end)
+		maps = get_maps_count(start, end, cs2, lan)
+		matches = get_matches_count(start, end, cs2, lan)
 		data.append({
 			"date": start,
 			"match_count" : matches,
@@ -133,24 +150,56 @@ def records_per_week():
 
 	return df
 
-def map_distribution():
-	map_counts = session.query(Map.map_name, func.count(Map.id)).group_by(Map.map_name).all()
+def map_distribution(cs2 = None, lan = None, min_rank = None, start_date = None, end_date = None):
+	# Filter Maps based on Match.cs2
+	query = session.query(Map.map_name, func.count(Map.id)).group_by(Map.map_name)
+
+	if cs2 is not None or lan is not None:
+		query = query.join(Match, Map.match_id == Match.id)
+
+	if cs2 is not None:
+		query = query.filter(Match.cs2 == 1 if cs2 else Match.cs2 == 0)
+
+	if lan is not None:
+		query = query.filter(Match.lan == 1 if lan else Match.lan == 0)
+		
+	map_counts = query.all()
 
 	df = pd.DataFrame(map_counts, columns=["map_name", "count"])
 	df = df.sort_values(by="count", ascending=False)
 
 	return df
 
-def format_distribution():
-	best_of_counts = session.query(Match.best_of, func.count(Match.id)).group_by(Match.best_of).all()
+def format_distribution(cs2 = None, lan = None):
+	query = session.query(Match.best_of, func.count(Match.id)).group_by(Match.best_of)
+
+	if cs2 is not None:
+		query = query.filter(Match.cs2 == 1 if cs2 else Match.cs2 == 0)
+
+	if lan is not None:
+		query = query.filter(Match.lan == 1 if lan else Match.lan == 0)
+
+	best_of_counts = query.all()
 
 	df = pd.DataFrame(best_of_counts, columns = ["format", "count"])
 
 	return df
 
-def rank_distribution():
-	team1_rank_counts = session.query(Match.team1_rank, func.count(Match.id)).group_by(Match.team1_rank).all()
-	team2_rank_counts = session.query(Match.team2_rank, func.count(Match.id)).group_by(Match.team2_rank).all()
+def rank_distribution(cs2 = None, lan = None):
+
+	query1 = session.query(Match.team1_rank, func.count(Match.id)).group_by(Match.team1_rank)
+	query2 = session.query(Match.team2_rank, func.count(Match.id)).group_by(Match.team2_rank)
+
+	if cs2 is not None:
+		query1 = query1.filter(Match.cs2 == 1 if cs2 else Match.cs2 == 0)
+		query2 = query2.filter(Match.cs2 == 1 if cs2 else Match.cs2 == 0)
+
+	if lan is not None:
+		query1 = query1.filter(Match.lan == 1 if lan else Match.lan == 0)
+		query2 = query2.filter(Match.lan == 1 if lan else Match.lan == 0)
+
+	team1_rank_counts = query1.all()
+	team2_rank_counts = query2.all()
 
 	team1_rank_df = pd.DataFrame(team1_rank_counts, columns=["team_rank", "count"])
 	team2_rank_df = pd.DataFrame(team2_rank_counts, columns=["team_rank", "count"])
@@ -160,8 +209,16 @@ def rank_distribution():
 
 	return result_df
 
-def rank_differential_distribution():
-	matches = session.query(Match.team1_rank, Match.team2_rank).filter(Match.team1_rank.isnot(None), Match.team2_rank.isnot(None)).all()
+def rank_differential_distribution(cs2 = None, lan = None):
+	query = session.query(Match.team1_rank, Match.team2_rank).filter(Match.team1_rank.isnot(None), Match.team2_rank.isnot(None))
+
+	if cs2 is not None:
+		query = query.filter(Match.cs2 == 1 if cs2 else Match.cs2 == 0)
+
+	if lan is not None:
+		query = query.filter(Match.lan == 1 if lan else Match.lan == 0)
+
+	matches = query.all()
 
 	differences = [abs(team1_rank - team2_rank) for team1_rank, team2_rank in matches]
 	difference_counts = {}
@@ -176,14 +233,17 @@ def rank_differential_distribution():
 
 	return result_df
 
-def score_distribution(cs2=False):
+def score_distribution(cs2 = None, lan = None):
 	query = session.query(Map.t1_score, Map.t2_score, func.count(Map.id)).group_by(Map.t1_score, Map.t2_score)
 
-	# Filter Maps based on Match.cs2
-	if cs2:
-		query = query.join(Match, Map.match_id == Match.id).filter(Match.cs2 == 1)
-	else:
-		query = query.join(Match, Map.match_id == Match.id).filter(Match.cs2 == 0)
+	if cs2 is not None or lan is not None:
+		query = query.join(Match, Map.match_id == Match.id)
+
+	if cs2 is not None:
+		query = query.filter(Match.cs2 == 1 if cs2 else Match.cs2 == 0)
+
+	if lan is not None:
+		query = query.filter(Match.lan == 1 if lan else Match.lan == 0)
 	
 	score_combinations = query.all()
 	df = pd.DataFrame(score_combinations, columns=["t1_score", "t2_score", "count"])
@@ -193,6 +253,34 @@ def score_distribution(cs2=False):
 	result_df.rename(columns={"score_pair": "Score", "count": "Count"}, inplace=True)
 
 	return result_df
+
+def winner_rank_diff_dist(cs2 = None, lan = None):
+    # Get team ids, ranks, winner_id. Calculate winning team rank - losing team rank
+    query = session.query(Match.team1_rank, Match.team2_rank, Match.winner, Match.team1_id).filter(
+        Match.team1_rank.isnot(None), Match.team2_rank.isnot(None), Match.winner.isnot(None)
+    )
+
+    if cs2 is not None:
+        query = query.filter(Match.cs2 == 1 if cs2 else Match.cs2 == 0)
+
+    if lan is not None:
+        query = query.filter(Match.lan == 1 if lan else Match.lan == 0)
+
+    matches = query.all()
+
+    differences = [team2_rank - team1_rank if winner == team1_id else team1_rank - team2_rank for team1_rank, team2_rank, winner, team1_id in matches]
+    difference_counts = {}
+    
+    for diff in differences:
+        if diff not in difference_counts:
+            difference_counts[diff] = 1
+        else:
+            difference_counts[diff] += 1
+
+    result_df = pd.DataFrame(list(difference_counts.items()), columns=["Rank_Difference", "Frequency"])
+    result_df = result_df.sort_values(by="Rank_Difference")
+
+    return result_df
 
 def plot_maps_per_month():
 	data = records_per_month()
@@ -228,16 +316,18 @@ def plot_maps_per_week():
 if __name__ == "__main__":
 	pd.set_option("display.max_rows", None)
 
-	# print(map_distribution())
-
-	# print(records_per_month())
+	# print(records_per_month(cs2=False, lan=True))
 
 	# print(records_per_week())
 
-	# print(format_distribution())
+	# print(map_distribution(cs2=True))
 
-	# print(rank_distribution())
+	# print(format_distribution(cs2 = True))
 
-	# print(rank_differential_distribution())
+	# print(rank_distribution(cs2 = False, lan = True))
 
-	print(score_distribution(cs2=False))
+	# print(rank_differential_distribution(lan = True))
+
+	# print(score_distribution(cs2=False, lan = True))
+
+	print(winner_rank_diff_dist(cs2 = False))
