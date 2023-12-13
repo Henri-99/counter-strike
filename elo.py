e3cc7f77689dc78e781cd521939275bb5e5cb028
Henri du Plessis <henriduplessis99@gmail.com>
2023-12-13 11:23:17 +0200

    Significant progress on ELO system

--- elo.py
+++ elo.py
@@ -11,101 +11,137 @@ import pandas as pd
 from datetime import datetime, timedelta
 from sqlalchemy import func, and_
 
-def insert_player_elo(player_id, map_id, date, elo, maps_played):
-    new_player_elo = PlayerElo(player_id=player_id, map_id=map_id, date=date, elo=elo, maps_played=maps_played)
-    session.add(new_player_elo)
-    session.commit()
+def insert_player_elos(player_elo_list):
+	try:
+		session.add_all(player_elo_list)
+		session.commit()
+		print("Records inserted successfully.")
+	except Exception as e:
+		session.rollback()
+		print(f"Error inserting records: {e}")
 
 def get_player_elos(lineup):
-    # Extract player IDs from the lineup
-    players = [lineup.player1_id, lineup.player2_id, lineup.player3_id, lineup.player4_id, lineup.player5_id]
+	# Extract player IDs from the lineup
+	players = [lineup.player1_id, lineup.player2_id, lineup.player3_id, lineup.player4_id, lineup.player5_id]
 
-    latest_map_ids = (
-        session.query(PlayerElo.player_id, func.max(PlayerElo.map_id).label('max_map_id'))
-        .filter(PlayerElo.player_id.in_(players))
-        .group_by(PlayerElo.player_id)
-        .subquery()
-    )
+	latest_map_ids = (
+		session.query(PlayerElo.player_id, func.max(PlayerElo.map_id).label('max_map_id'))
+		.filter(PlayerElo.player_id.in_(players))
+		.group_by(PlayerElo.player_id)
+		.subquery()
+	)
 
-    # Query to get player ELOs for the latest map_id
-    elo_query = (
-        session.query(PlayerElo.player_id, PlayerElo.elo, PlayerElo.maps_played)
-        .join(latest_map_ids, and_(PlayerElo.player_id == latest_map_ids.c.player_id, PlayerElo.map_id == latest_map_ids.c.max_map_id))
-    )
+	# Query to get player ELOs for the latest map_id
+	elo_query = (
+		session.query(PlayerElo.player_id, PlayerElo.elo, PlayerElo.maps_played, PlayerElo.date)
+		.join(latest_map_ids, and_(PlayerElo.player_id == latest_map_ids.c.player_id, PlayerElo.map_id == latest_map_ids.c.max_map_id))
+	)
 
-    # Fetch all results in one query
-    results = elo_query.all()
+	# Fetch all results in one query
+	results = elo_query.all()
 
-    # Create a list of dictionaries for each player
-    player_elos = [
-        {'player_id': result.player_id, 'elo': result.elo, 'maps_played': result.maps_played}
-        for result in results
-    ]
+	# Create a list of dictionaries for each player
+	player_elos = [
+		{'player_id': result.player_id, 'elo': result.elo, 'maps_played': result.maps_played, 'date' : result.date}
+		for result in results
+	]
 
-    return player_elos
+	return player_elos
 
 def calculate_expected_outcome(rating1, rating2):
-    expected_outcome1 = 1 / (1 + 10**((rating2 - rating1) / 400))
-    expected_outcome2 = 1 / (1 + 10**((rating1 - rating2) / 400))
-    return expected_outcome1, expected_outcome2
+	expected_outcome1 = 1 / (1 + 10**((rating2 - rating1) / 400))
+	expected_outcome2 = 1 / (1 + 10**((rating1 - rating2) / 400))
+	return expected_outcome1, expected_outcome2
 
-def update_ratings(team_ratings1, team_ratings2, k_factor, outcome1, outcome2):
-    updated_ratings1 = [
-        rating + k_factor * sum(outcome1 - calculate_expected_outcome(team_ratings1, team_ratings2)[0] for other_rating in team_ratings2)
-        for rating in team_ratings1
-    ]
-    updated_ratings2 = [
-        rating + k_factor * sum(outcome2 - calculate_expected_outcome(team_ratings1, team_ratings2)[1] for other_rating in team_ratings1)
-        for rating in team_ratings2
-    ]
-    return updated_ratings1, updated_ratings2
+def update_ratings(team_ratings1, team_ratings2, expected_outcome_A, expected_outcome_B, k_factor, winner):
+	updated_ratings1 = [
+		player['elo'] + k_factor * (winner - expected_outcome_A)
+		for player in team_ratings1
+	]
+	updated_ratings2 = [
+		player['elo'] + k_factor * (1-winner - expected_outcome_B)
+		for player in team_ratings2
+	]
+	return updated_ratings1, updated_ratings2
 
 def add_new_players(lineup):
-    player_id_list = [lineup.player1_id, lineup.player2_id, lineup.player3_id, lineup.player4_id, lineup.player5_id]
-    existing_players = session.query(PlayerElo.player_id).filter(PlayerElo.player_id.in_(player_id_list)).all()
-    existing_players = {player[0] for player in existing_players}
+	player_id_list = [lineup.player1_id, lineup.player2_id, lineup.player3_id, lineup.player4_id, lineup.player5_id]
+	existing_players = session.query(PlayerElo.player_id).filter(PlayerElo.player_id.in_(player_id_list)).all()
+	existing_players = {player[0] for player in existing_players}
 
-    # Identify new players
-    new_players = [player_id for player_id in player_id_list if player_id not in existing_players]
+	# Identify new players
+	new_players = [player_id for player_id in player_id_list if player_id not in existing_players]
 
-    # Insert records for new players
-    for player_id in new_players:
-        insert_player_elo(player_id=player_id, map_id=0, date=datetime.strptime(lineup.date, "%Y-%m-%d %H:%M"), elo=1000.0, maps_played=0)
+
+	new_player_elos = []
+	# Insert records for new players
+	for player_id in new_players:
+		new_player_elos.append(PlayerElo(player_id=player_id, map_id=0, date=datetime.strptime(lineup.date, "%Y-%m-%d %H:%M"), elo=1000.0, maps_played=0))
+	insert_player_elos(new_player_elos)
 
 def calculate_team_elo(elo_list):
-    total_elo = 0
-    for player in elo_list:
-        total_elo += player['elo']
-    team_elo = total_elo / 5
-    return team_elo
+	total_elo = 0
+	for player in elo_list:
+		total_elo += player['elo']
+	team_elo = total_elo / 5
+	return team_elo
 
 def update_elos(map_):
-    # Get lineups for given match
-    lineup_A, lineup_B = session.query(Lineup).filter(map_.Match.id == Lineup.match_id).all()
+	# Get lineups for given match
+	lineup_A, lineup_B = session.query(Lineup).filter(map_.Match.id == Lineup.match_id).all()
 
-    # Initiate any new players to the database
-    add_new_players(lineup_A)
-    add_new_players(lineup_B)
+	# Initiate any new players to the database
+	add_new_players(lineup_A)
+	add_new_players(lineup_B)
 
-    # Get player ELO dicts for both teams
-    elo_list_A = get_player_elos(lineup_A)
-    elo_list_B = get_player_elos(lineup_B)
+	# Get player ELO dicts for both teams: [ {'player_id': 1950, 'elo': 1000.0, 'maps_played': 0}, ... ]
+	elo_list_A = get_player_elos(lineup_A)
+	elo_list_B = get_player_elos(lineup_B)
 
-    # Calculate expected result
-    probA, probB = calculate_expected_outcome(calculate_team_elo(elo_list_A), calculate_team_elo(elo_list_B))
+	print("Player ELOs before update: ")
+	print([f"{player['player_id']}:{player['elo']}" for player in elo_list_A])
+	print([f"{player['player_id']}:{player['elo']}" for player in elo_list_B])
 
-    # Get actual result
-    map_outcome = map_. 
+	# Calculate expected result
+	probA, probB = calculate_expected_outcome(calculate_team_elo(elo_list_A), calculate_team_elo(elo_list_B))
+
+	# Get actual result
+	map_outcome = 1 if map_.Map.t1_id == map_.Map.winner_id else 0
+
+	# Compute new ELOs
+	new_elo_list_A, new_elo_list_B = update_ratings(elo_list_A, elo_list_B, probA, probB, 10, map_outcome)
+
+	# Insert new ELOs into database
+	elo_objects = []
+
+	for i, player in enumerate(elo_list_A):
+		elo_objects.append(PlayerElo(map_id = map_.Map.id, 
+							   player_id = player['player_id'], 
+							   date = datetime.strptime(map_.Map.datetime, "%Y-%m-%d %H:%M"), 
+							   elo = new_elo_list_A[i], 
+							   maps_played = player['maps_played'] + 1))
+	for i, player in enumerate(elo_list_B):
+		elo_objects.append(PlayerElo(map_id = map_.Map.id, 
+							   player_id = player['player_id'], 
+							   date = datetime.strptime(map_.Map.datetime, "%Y-%m-%d %H:%M"), 
+							   elo = new_elo_list_B[i], 
+							   maps_played = player['maps_played'] + 1))
+	
+	insert_player_elos(elo_objects)
 
 
+	elo_list_A = get_player_elos(lineup_A)
+	elo_list_B = get_player_elos(lineup_B)
+	print("Player ELOs after update: ")
+	print([f"{player['player_id']}:{player['elo']}" for player in elo_list_A], end = " and ")
+	print([f"{player['player_id']}:{player['elo']}" for player in elo_list_B])
+	print("\n")
 
-    return 0
 
-query = session.query(Map, Match).join(Match, Map.match_id == Match.id, isouter=True)
+if __name__ == "__main__":
+	# Get some maps
+	query = session.query(Map, Match).join(Match, Map.match_id == Match.id, isouter=True)
+	map_objects = query.limit(1000).all()
 
-map_objects = query.limit(10).all()
-
-for map_ in map_objects:
-    update_elos(map_)
-
-    break
+	for map_ in map_objects:
+		update_elos(map_)
